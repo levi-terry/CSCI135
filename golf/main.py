a056ebdf0cc534daed833dba422b812966aba141
Levi Terry <ldterry14@gmail.com>
2018-10-28 12:46:30 -0600

    Added golf program and text file

---
+++ golf/main.py
@@ -0,0 +1,9 @@
+"""
+Design and implement a program to process golf scores.
+The scores of four golfers are stored in a file called golf.txt
+Each line represents one hole, and the file contains 18 lines.
+Each line contains five values: par for the hole followed by the number of strokes each golfer used on that hole.
+Store the total for par in the variable par.
+Store the totals for the players in a list called players.
+Determine the winner and produce a table showing how well each golfer did.
+"""
