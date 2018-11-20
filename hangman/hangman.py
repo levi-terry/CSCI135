2f492d05c467e93d71df2849582102e52b9bdb6b
Levi Terry <ldterry14@gmail.com>
2018-11-20 12:27:14 -0700

    Made some edits in hangman

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,29 +1,37 @@
 import random
-from hangman_drawing import display
+import math
+from hangman_drawing import draw_display
 
 player_continue = "y"
 word_list = ["longing", "rusted", "furnace", "daybreak", "seventeen", "benign", "nine", "homecoming", "one",
              "freight car"]
 player_guesses = 0
-print(display[0])
 
 while player_continue != "n":
     word = random.choice(word_list)
     print(word)
-    player_guesses = len(word)
+    player_guesses = math.ceil(len(word) / 2)
     display_word = "*" * len(word)
-    missed_letters = []
+    missed_letters = ''
+    correct_letters = ''
     print(display_word)
 
-    while player_guesses > 0:
+    while player_guesses != 6:
         player_choice = input("Please guess a letter: ")
         for letter in word:
             if player_choice in word:
-                print(letter)
+                correct_letters += player_choice
+                draw_display(player_guesses=player_guesses, missed_letters=missed_letters,
+                             correct_letters=correct_letters)
                 break
             else:
-                missed_letters.append(player_choice)
-                print("Missed Letters:\n", missed_letters)
-                player_guesses -= 1
+                missed_letters += player_choice
+                player_guesses += 1
+                draw_display(player_guesses=player_guesses, missed_letters=missed_letters,
+                             correct_letters=correct_letters)
+                break
 
-    player_continue = input("Play again? (n to stop)\n")
+    if player_guesses == 0:
+        print("You lose.")
+
+    player_continue = input("Play again? (n to quit)\n")
--- hangman/hangman_drawing.py
+++ hangman/hangman_drawing.py
@@ -60,3 +60,11 @@ display = ['''
           |
      _____|
            ''']
+
+
+def draw_display(player_guesses, missed_letters, correct_letters):
+    print(display[player_guesses])
+    print("Missed Letters: ")
+    for letter in missed_letters:
+        print(letter, end=' ')
+    print("\nGuesses Left: %d" % player_guesses)
