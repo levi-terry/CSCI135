06e8d0e240e404ca8f1adbe7529635547cffc055
Levi Terry <ldterry14@gmail.com>
2018-11-21 10:44:36 -0700

    Now have a functional version of hangman

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,37 +1,48 @@
 import random
-import math
-from hangman_drawing import draw_display
+from hangman_drawing import draw_display, replace_letter
 
 player_continue = "y"
 word_list = ["longing", "rusted", "furnace", "daybreak", "seventeen", "benign", "nine", "homecoming", "one",
              "freight car"]
-player_guesses = 0
 
 while player_continue != "n":
-    word = random.choice(word_list)
+    # Display welcome message to player, repeats with every new game
+    print("*" * 23)
+    print("* Welcome to Hangman! *")
+    print("*" * 23)
+    # Pick random word from list, save that string as a list
+    word = list(random.choice(word_list))
+    # display word, for testing // remove later
     print(word)
-    player_guesses = math.ceil(len(word) / 2)
-    display_word = "*" * len(word)
-    missed_letters = ''
-    correct_letters = ''
-    print(display_word)
+    # Initialize player guesses, display word, missed letters
+    player_guesses = 0
+    display_word = list("-" * len(word))
+    missed_letters = []
 
     while player_guesses != 6:
+        if display_word == word:
+            break
         player_choice = input("Please guess a letter: ")
+        # Loop to not allow repeat guesses of same letter
+        while player_choice in missed_letters or player_choice in display_word:
+            print("You have already guessed the letter '%c'." % player_choice)
+            player_choice = input("Please guess another letter: ")
         for letter in word:
             if player_choice in word:
-                correct_letters += player_choice
+                display_word = replace_letter(word, display_word, player_choice)
                 draw_display(player_guesses=player_guesses, missed_letters=missed_letters,
-                             correct_letters=correct_letters)
+                             display_word=display_word)
                 break
             else:
                 missed_letters += player_choice
                 player_guesses += 1
                 draw_display(player_guesses=player_guesses, missed_letters=missed_letters,
-                             correct_letters=correct_letters)
+                             display_word=display_word)
                 break
 
-    if player_guesses == 0:
+    if display_word == word:
+        print("You win!")
+    if player_guesses == 6:
         print("You lose.")
 
     player_continue = input("Play again? (n to quit)\n")
--- hangman/hangman_drawing.py
+++ hangman/hangman_drawing.py
@@ -62,9 +62,19 @@ display = ['''
            ''']
 
 
-def draw_display(player_guesses, missed_letters, correct_letters):
+def draw_display(player_guesses, display_word, missed_letters):
     print(display[player_guesses])
-    print("Missed Letters: ")
+    print("Word: ", end='')
+    for letter in display_word:
+        print(letter, end='')
+    print("\nMissed Letters: ")
     for letter in missed_letters:
         print(letter, end=' ')
-    print("\nGuesses Left: %d" % player_guesses)
+    print()
+
+
+def replace_letter(word, display_word, player_guess):
+    for position, char_a in enumerate(word):
+        if player_guess == char_a:
+            display_word[position] = char_a
+    return display_word
